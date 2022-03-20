dcb2758715c4fee51f180c6469fdd22b065d3343
yuval <nhajeho1@gmail.com>
2022-03-20 14:14:37 +0200

    worldpin now works

--- scripts/worldPin.gd
+++ scripts/worldPin.gd
@@ -3,14 +3,27 @@ extends Node2D
 func _draw():
 	draw_circle(Vector2(), 10, Color(1,1,1))
 
-#func _ready():
-#	yield(get_tree(), "physics_frame")
-#	yield(get_tree(), "physics_frame")
-#	print($Area2D.get_overlapping_bodies())
+func _ready():
+	yield(get_tree(), "physics_frame")
+	yield(get_tree(), "physics_frame")
+	var pinned_object = $Area2D.get_overlapping_bodies()
+	if not pinned_object:
+		queue_free()
+		return
+	
+	reparentPinned(pinned_object[0])
+
+func reparentPinned(obj: Node2D):
+#	get_parent().remove_child(self)
+#	var parent = obj.get_parent()
+#	parent.remove_child(obj)
+#	parent.add_child(self)
+#	add_child(obj)
+	$PinJoint2D.node_b=obj.get_path()
+	pass
 
 
-func _on_Area2D_body_entered(body):
-	print($Area2D.get_overlapping_bodies())
+func rem_area2D(): #just a tiny free and easy performance save ;-)
 	var rem = $Area2D
 	remove_child($Area2D)
 	rem.queue_free()
