b03261e7e79ef381766d79c75e2e801f12248bfd
yuval <nhajeho1@gmail.com>
2022-03-20 11:20:49 +0200

    added a pin with some functionality

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,17 @@
 extends Node
 var is_slomo = false
+var mouseMode = 1
+onready var pin = load("res://scenes/worldPin.tscn")
 
 func _input(event):
-	if event.is_action_pressed("ui_select"):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			if mouseMode == 1:
+				var newPin = pin.instance()
+				newPin.global_position = $Camera2D.get_global_mouse_position()
+				add_child(newPin)
+	
+	elif event.is_action_pressed("ui_select"):
 		if is_slomo:
 			Engine.time_scale = 1
 			is_slomo = false
@@ -15,4 +24,3 @@ func _input(event):
 	elif event.is_action_pressed("zoom_out"):
 		$Camera2D.global_position = $Camera2D.get_global_mouse_position()
 		$Camera2D.zoom *= 2
-		
