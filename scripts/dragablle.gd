0c606fb3ec66e428429edb27fa48e1c2e7ad291b
yuval <nhajeho1@gmail.com>
2022-03-22 09:10:22 +0200

    added linear damp to dragged objects

--- scripts/dragablle.gd
+++ scripts/dragablle.gd
@@ -16,10 +16,11 @@ func _input_event(_viewport, event, _shape_idx):
 func _physics_process(_delta):
 	var mp = get_global_mouse_position()
 	if is_held:
-		var dir_force = global_position.direction_to(mp)
-		dir_force *= global_position.distance_to(mp)
-		print(dir_force)
+		var dir_force = (mp-global_position)*follow_mouse_strength
+		linear_damp = 20
 		set_axis_velocity(dir_force)
+	else:
+		linear_damp = -1
 
 func pickup():
 	if is_held:
